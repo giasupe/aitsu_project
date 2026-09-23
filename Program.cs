4d9614ddf425fa1a27c1e5d989b5a998fe5f25da
giasupe <giasupe.com@gmail.com>
2026-09-23 22:14:24 +0900

    feature/view-model

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@ using System.Text.Json;
 
 using var HttpClient = new HttpClient();
 
+var tags = await HttpClient.GetAsync(
+    "http://localhost:11434/api/tags");
+tags.EnsureSuccessStatusCode();
+
+var tagsList = JsonDocument.Parse(
+    await tags.Content.ReadAsStringAsync());
+
+for (int i = 0; i < tagsList.RootElement.GetProperty("models").GetArrayLength(); i++)
+{
+    var outputModelList = tagsList.RootElement
+    .GetProperty("models")[i]
+    .GetProperty("name")
+    .GetString();
+    System.Console.WriteLine($"model: {outputModelList}");
+};
+
 while (true)
 {
     string? input = Console.ReadLine();
@@ -15,7 +31,7 @@ while (true)
         "http://localhost:11434/api/chat",
         new // 匿名オブジェクトを作成
         {
-            model = "llama3.2",
+            model = "gemma4:26b",
             messages = new[] // messagesに配列を入れる
             {
                 new // 配列に入れる匿名オブジェクトを作成
